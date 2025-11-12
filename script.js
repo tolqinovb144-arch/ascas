435d4e0d98b906006d2e672b08b4ad7b12cc8e34
tolqinovb144-arch <tolqinovb144@gmail.com>
2025-11-12 09:10:27 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-
-    const notifbox =document.getElementById("notif-box");
-
-
-
-    function korsat(rang,yozuv){
-        const div =document.createElement("div");
-        div.classList.add("notif");
-        div.style.backgroundColor=rang;
-        div.innerHTML=`
-            <p>${yozuv}</p>
-            <button class="closebtn">x</button>
-        `;
-        notifbox.prepend(div);
-    }
-    setTimeout(() => {
-        notifbox.removeChild(div);
-    }, 3000);
-
-    function ochir(e){
-        notifbox.removeChild(e.parentNode);
-    }
+const notifBox=document.getElementById("notif-box");
+function show(rang,yozuv){
+    const div=document.createElement("div");
+    div.classList.add("notif");
+    div.style.backgroundColor=rang;
+    div.innerHTML=`
+    <p>${yozuv} </p>
+    <button onclick="ochir(this)" class="close">x</button>
+    `
+    notifBox.prepend(div);
+    setTimeout(()=>{
+notifBox.removeChild(div);
+    },5000)
+}
+function ochir(e){
+notifBox.removeChild(e.parentNode)
+}
